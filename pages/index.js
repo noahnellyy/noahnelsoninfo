87d957293cf583217cb1f63e8d27aa288ad357e0
Noah Nelson <nelson.noah2002@gmail.com>
2023-08-11 17:56:47 -0400

    Grindr Update

--- pages/index.js
+++ pages/index.js
@@ -21,10 +21,14 @@ export default function Home() {
 
 
         <p>Hi! I&apos;m Noah &#x1F44B;</p>
-          <p className={styles.maintext}>I&apos;m a Junior student-athlete at The University of Chicago, studying Economics and Computer Science, alongside playing Varisty Baseball {"("} 
-          <a className={styles.baseball} href="https://athletics.uchicago.edu/sports/bsb/2021-22/bios/nelson_noah_c85d"><em>check me out!</em></a>{")"} I&apos;m also a member of Psi Upsilon!</p>
-          <p className={styles.maintext}>I previously worked as a Software Engineer Intern at <a href="https://riis.com/" className={styles.riis}><em>RIIS LLC</em></a>, where I developed applications specific to commerical drones!</p>
-        <p className={styles.maintext}>I&apos;m actively seeking Summer &apos;23 opportunities, my projects are below &#x2935; </p>
+          <p className={styles.maintext}>I&apos;m a Senior student-athlete at The University of Chicago, studying Economics and Computer Science, alongside playing Varsity Baseball {"("} 
+          <a className={styles.baseball} href="https://athletics.uchicago.edu/sports/bsb/2021-22/bios/nelson_noah_c85d"><em>check me out!</em></a>{")"} I&apos;m also the treasurer of Psi Upsilon!</p>
+          <p className={styles.maintext}>Summer '23 I had the opportunity to work as an <strong>Android Platform Engineer Intern</strong> at 
+          <a href="https://www.grindr.com/" className={styles.riis}><em> Grindr LLC</em></a>, during my time there I was actively working with and uploading code in production. I modernized the analytics infrastructure by integrating a deprecated analytics class with the current version, ensuring alignment with the MVI coding architecture and optimizing it through my understanding of dependency injection. Additionally, I led a significant project involving in-depth research, a complete redesign, and diligent testing to introduce a robust circuit breaker mechanism to enhance our application's dependability.
+
+</p>
+          <p className={styles.maintext}>Before Grindr, I previously worked as a <strong>Software Engineer Intern</strong> at <a href="https://riis.com/" className={styles.riis}><em>RIIS LLC</em></a>, where I developed applications specific to commerical drones!</p>
+        <p className={styles.maintext}>I&apos;m actively seeking full-time positions, my projects are below &#x2935; </p>
 
         <ul className={styles.Head}>
           <li><span className={styles.textLink}><a href="https://www.youtube.com/watch?v=BjOdJLOV0KY"> <em>Changing Drone Flight Modes Project</em></a></span></li>
@@ -35,12 +39,11 @@ export default function Home() {
 
         <p className={styles.inTouch}>Let&apos;s get in touch &#x2935; </p>
          
-          
           <div>
             <ul>
-              <li><span className={styles.textLink}><a className={styles.link} href="https://twitter.com/noahnellyy" target="_blank" rel="noreferrer"> <em>Twitter</em></a></span></li> 
-              <li><span className={styles.textLink}><a className={styles.link} href="https://www.linkedin.com/in/nnelson10/" target="_blank" rel="noreferrer"><em> LinkedIn</em></a></span></li>
-              <li><span className={styles.textLink}><a className={styles.link} href="https://read.cv/noahnellyy" target="_blank" rel="noreferrer"><em> ReadCV</em></a></span></li>
+              <li><span className={styles.textLink}><a className={styles.link} href="https://www.linkedin.com/in/nnelson10/" target="_blank" rel="noreferrer"><em>LinkedIn</em></a></span></li>
+              <li><span className={styles.textLink}><a className={styles.link} href="https://twitter.com/noahnellyy" target="_blank" rel="noreferrer"><em>Twitter</em></a></span></li> 
+              <li><span className={styles.textLink}><a className={styles.link} href="https://read.cv/noahnellyy" target="_blank" rel="noreferrer"><em>ReadCV</em></a></span></li>
             </ul>
          </div>
 
