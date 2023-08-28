a97e846afe84962bc93d0ef8343964e583693ef2
Noah Nelson <nelson.noah2002@gmail.com>
2023-08-28 13:53:20 -0400

    Grindr Update

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,7 @@ export default function Home() {
           <p className={styles.maintext}>I&apos;m a Senior student-athlete at The University of Chicago, studying Economics and Computer Science, alongside playing Varsity Baseball {"("} 
           <a className={styles.baseball} href="https://athletics.uchicago.edu/sports/bsb/2021-22/bios/nelson_noah_c85d"><em>check me out!</em></a>{")"} I&apos;m also the treasurer of Psi Upsilon!</p>
           <p className={styles.maintext}>Summer &apos;23 I had the opportunity to work as an <strong>Android Platform Engineer Intern</strong> at 
-          <a href="https://www.grindr.com/" className={styles.riis}><em> Grindr LLC</em></a>, during my time there I was actively working with and uploading code in production. I modernized the analytics infrastructure by integrating a deprecated analytics class with the current version, ensuring alignment with the MVI coding architecture and optimizing it through my understanding of dependency injection. Additionally, I led a significant project involving in-depth research, a complete redesign, and diligent testing to introduce a robust circuit breaker mechanism to enhance our application&apos;s dependability.</p>
+          <a href="https://www.grindr.com/" className={styles.riis}><em> Grindr</em></a>, during my time there I was actively working with and uploading code in production. I modernized the analytics infrastructure by integrating a deprecated analytics class with the current version, optimizing it through my understanding of dependency injection and MVVM. Additionally, I led a significant project involving in-depth research, a complete redesign, and diligent testing to introduce a robust circuit breaker mechanism to enhance our application&apos;s dependability.</p>
           <p className={styles.maintext}>Before Grindr, I previously worked as a <strong>Software Engineer Intern</strong> at <a href="https://riis.com/" className={styles.riis}><em>RIIS LLC</em></a>, where I developed applications specific to commerical drones!</p>
           <p className={styles.maintext}>I&apos;m actively seeking full-time positions, my projects are below &#x2935; </p>
 
@@ -47,6 +47,12 @@ export default function Home() {
 
   
       </div>
+      <div className={styles.bottomImages}>
+        <Image className={styles.grindr} src="/download-3.jpg" width={300} height={168} alt="Grindr" />
+        <div className={styles.imageSpace}></div>
+        <Image className={styles.riis} src="/riis.jpg" width={168} height={168} alt="RIIS" />
+      </div>
+      
     </div>
   )
 }
